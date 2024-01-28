c2994f3457f8946b179bdb77c805c0842e6380be
Yrll21 <ivanyrlllabadia@gmail.com>
2024-01-28 23:16:47 +0800

    Set up video processing service locally

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+
+const app = express()
+
+app.post("/process-video", (req,res) => {
+    const inputFilePath = req.body.inputFilePath
+    const outputFilePath = req.body.outputFilePath
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing File Path.")
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") //360p
+        .on("end", () => {
+            res.status(200).send("Processing finished successfully")
+        })
+        .on("error", (err) => {
+            console.log(`An error occured: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`)
+        })
+        .save(outputFilePath)
+});
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(
+        `Listening at port: ${port}`
+    )
+})

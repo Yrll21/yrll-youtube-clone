2394a9e96f880ae9f8a7e28f88bbc60363a0eeb2
John Doe <ivanyrlllabadia@gmail.com>
2024-05-03 16:56:34 +0800

    process videos locally. Added endpoint

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,14 +1,35 @@
 // import express
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 // create express app
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-// get request
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// post request
+app.post("/process-video", (req, res) => {
+  // get path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing input or output file path");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") // 360p
+    .on("end", () => {
+      res.status(200).send("Video processing finished successfully");
+    })
+    .on("error", (err) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send("Internal Server Error" + err.message);
+    })
+    .save(outputFilePath);
 });
+
+// declare port
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Video processing service listening at http://localhost:${port}`);

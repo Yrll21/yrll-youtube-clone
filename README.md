836fa5a255558a08e040dc7f9c712f635fee491d
John Doe <ivanyrlllabadia@gmail.com>
2024-05-06 13:30:00 +0800

    add sign in with google

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # yrll-youtube-clone`
+
+# To Do's
+
+## Video Processing Service
+
+- [x] Initialize Video Processing Service
+- [x] Process Video Locally
+- [x] Containerize Video Processing Service
+- [x] Convert Video Hosted on Google Cloud Storage
+
+## Google Cloud
+
+- [x] Deploy Video Processing Service
+- [x] Create Pub/Sub Topic and Subscription
+- [x] Create Cloud Storage Buckets
+
+## Web App
+
+- [x] Create Next App
+- [x] Create Navbar
+- [x] Add Firebase SDK with Auth
+- [ ] Add Sign In with Google
--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,20 +1,30 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "./navbar.module.css";
 import Link from "next/link";
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
 
 const NavBar = () => {
+  // Init user state
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+    // cleanup subscription on unmount
+    return () => unsubscribe();
+  });
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <Image
-          className={styles.logo}
-          src="/youtube-logo.svg"
-          alt="logo"
-          width={20}
-          height={90}
-        />
+        <Image src="/youtube-logo.svg" alt="logo" width={90} height={90} />
       </Link>
+      {/* TODO: Add upload button if user is signed in */}
+      <SignIn user={user} />
     </nav>
   );
 };

13c635bb2e38bd35581a1cb1386610b5fcece6ef
Yrll <101014492+Yrll21@users.noreply.github.com>
2024-05-14 23:53:24 +0000

    Updated Navbar to remove h1

--- yt-web-client/app/navbar/upload.tsx
+++ yt-web-client/app/navbar/upload.tsx
@@ -24,7 +24,6 @@ export default function Upload() {
 
   return (
     <Fragment>
-    <h1>Upload Video</h1>
       <input id="upload" className={styles.uploadInput} type="file" accept="video/*" onChange={handleFileChange} />
       <label htmlFor="upload" className={styles.uploadButton}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor" className="w-6 h-6">
